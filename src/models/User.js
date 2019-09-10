5001c700d1ae7d5428bee88cf09e0c9e91b26151
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2019-09-10 15:49:44 -0300

    basic of User and Server models

---
+++ src/models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  level: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+mongoose.model('User', UserSchema);
